59759a2e87530399418a37f3149990ebad75445b
AlfredoMartinezMartinez <32301788+AlfredoMartinezMartinez@users.noreply.github.com>
2017-11-30 23:04:45 +0100

    v1.2
    funcion comprar, solo esqueleto, no esta implmentado

---
+++ Spotif/src/main/java/Dominio/Gestor_pasarela.java
@@ -0,0 +1,14 @@
+package Dominio;
+
+public class Gestor_pasarela {
+
+	/**
+	 * 
+	 * @param numero_cuenta
+	 */
+	public boolean realizarCompra(int numero_cuenta) {
+		// TODO - implement Gestor_pasarela.realizarCompra
+		throw new UnsupportedOperationException();
+	}
+
+}
