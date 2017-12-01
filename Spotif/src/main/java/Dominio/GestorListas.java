aa126955a4a35519d25354dc81fdb08db03fa9c1
AlfredoMartinezMartinez <32301788+AlfredoMartinezMartinez@users.noreply.github.com>
2017-12-01 05:42:46 +0100

    v1.8
    casos de uso borrar y modificar listas

---
+++ Spotif/src/main/java/Dominio/GestorListas.java
@@ -0,0 +1,14 @@
+package Dominio;
+
+public class GestorListas {
+
+	/**
+	 * 
+	 * @param musica
+	 */
+	public boolean gestionarListas(Musica musica) {
+		// TODO - implement GestorListas.gestionarListas
+		throw new UnsupportedOperationException();
+	}
+
+}
